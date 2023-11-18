69d1ddedbea8a187b2aaac9c3651f7b499ed2b27
chilan666 <3108727593@qq.com>
2023-11-18 15:15:52 +0800

    first upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_zijin.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_zijin-user \
+    omni_zijin-userdebug \
+    omni_zijin-eng
